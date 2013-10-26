2acc678199e467d86d7346ef117a01b660da892c
Jan Machacek <jan.machacek@gmail.com>
2013-10-26 11:36:31 +0100

    Proper tests for the MessageQueueSize and MessageCount. Close #11.

--- agent-akka/src/test/scala/org/eigengo/monitor/agent/akka/TestCounterInterface.scala
+++ agent-akka/src/test/scala/org/eigengo/monitor/agent/akka/TestCounterInterface.scala
@@ -3,39 +3,123 @@ package org.eigengo.monitor.agent.akka
 import org.eigengo.monitor.output.CounterInterface
 import scala.collection.mutable
 
+/**
+ * Test-only implementation of the ``CounterInterface`` that records all received
+ * counters, gauges and others in its companion object, which you can then use
+ * in your tests to make assertions.
+ */
 class TestCounterInterface extends CounterInterface {
 
-  def decrementCounter(name: String, tags: String*): Unit = {
+  override def decrementCounter(name: String, tags: String*): Unit = {
     TestCounterInterface.add(name, -1, tags.toList)
   }
 
-  def incrementCounter(name: String, tags: String*): Unit = {
+  override def incrementCounter(name: String, tags: String*): Unit = {
     TestCounterInterface.add(name, 1, tags.toList)
   }
 
-  def recordGaugeValue(aspect: String, value: Int, tags: String*): Unit = {
+  override def recordGaugeValue(aspect: String, value: Int, tags: String*): Unit = {
     TestCounterInterface.set(aspect, value, tags.toList)
   }
 }
 
-case class TestCounter(value: Int, tags: List[String] = Nil) {
-  def +(delta: Int): TestCounter = TestCounter(value + delta, tags)
+/**
+ * An entry received by the counter interface, for example _statsd_
+ *
+ * @param aspect the counter aspect (i.e. group name)
+ * @param value the counter value
+ * @param tags the optional tags
+ */
+case class TestCounter(aspect: String, value: Int, tags: List[String] = Nil)
+object TestCounter {
+  // asserts that the two counters are compatible
+  private def assertAspects(a: TestCounter, b: TestCounter): Unit = {
+    if (a.aspect != b.aspect) throw new RuntimeException(s"${a.aspect} != ${b.aspect}")
+  }
+
+  /**
+   * Adds the values of two counters
+   *
+   * @param a the first counter
+   * @param b the second counter
+   * @return a + b
+   */
+  def plus(a: TestCounter, b: TestCounter): TestCounter = {
+    assertAspects(a, b)
+    a.copy(value = a.value + b.value)
+  }
+
+  /**
+   * Returns the greater of the two counters
+   *
+   * @param a the first counter
+   * @param b the second counter
+   * @return the greater of {a, b}
+   */
+  def max(a: TestCounter, b: TestCounter): TestCounter = {
+    assertAspects(a, b)
+    if (a.value > b.value) a else b
+  }
+
 }
 
+/**
+ * Specifies tag filtering to be either AnyTag, SingleTag
+ */
+sealed trait TagFilter
+case object AnyTag extends TagFilter
+case class SingleTag(tag: String) extends TagFilter
+// case class AllTags(tags: List[String]) extends TagFilter
+
+/**
+ * Companion for the TestCounterInterface containing all recorded events
+ */
 object TestCounterInterface {
-  val counters = new mutable.HashMap[String, TestCounter]()
+  private val counters = new mutable.ListBuffer[TestCounter]()
 
-  def add(aspect: String, delta: Int, tags: List[String]): Unit = {
-    val newCount = counters.getOrElse(aspect, TestCounter(0, tags)) + delta
-    counters.put(aspect, newCount)
+  // adds a new counter
+  private def add(aspect: String, delta: Int, tags: List[String]): Unit = {
+    val counter: TestCounter = TestCounter(aspect, delta, tags)
+    counters += counter
 
-    println(s"**counter: $aspect -> $newCount")
+    println(s"**counter: $aspect -> $counter")
   }
 
-  def set(aspect: String, value: Int, tags: List[String]): Unit = {
-    val gauge = TestCounter(value, tags)
-    counters.put(aspect, gauge)
+  // sets a gauge
+  private def set(aspect: String, value: Int, tags: List[String]): Unit = {
+    val gauge = TestCounter(aspect, value, tags)
+    counters += gauge
+
     println(s"**gauge:   $aspect -> $gauge")
   }
+
+  /**
+   * Selects from the recorded counters those with the matching ``aspect`` and conforming to the
+   * requested ``tagFilter``, and then folds the matches over some operation ``fold``.
+   *
+   * @param aspect the aspect to be matched
+   * @param tagFilter the tag filter
+   * @param fold the fold operation
+   * @return list of matching & folded tags
+   */
+  def foldlByAspect(aspect: String, tagFilter: TagFilter = AnyTag)(fold: (TestCounter, TestCounter) => TestCounter): List[TestCounter] = {
+    def pred(x: TestCounter): Boolean =
+      (x.aspect == aspect) &&
+      (tagFilter match {
+        case AnyTag         => true
+        case SingleTag(tag) => x.tags.size == 1 && x.tags.head == tag
+      })
+
+    counters.foldLeft[List[TestCounter]](Nil) { (b, a) =>
+      if (a.aspect == aspect) b.find(pred).map(fold(a, _)).getOrElse(a) :: b else b
+    }
+  }
+
+  /**
+   * Returns immutable view of all recorded counters
+   *
+   * @return all recorded counters
+   */
+  def allCounters: List[TestCounter] = counters.toList
 
 }
